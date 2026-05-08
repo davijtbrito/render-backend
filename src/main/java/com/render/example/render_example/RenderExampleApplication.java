1d9a9de8170d3b0208fb0f85b1081aaedc2c303a
davijtbrito <47305119+davijtbrito@users.noreply.github.com>
2026-05-08 21:59:05 +0100

    initial implementation

---
+++ src/main/java/com/render/example/render_example/RenderExampleApplication.java
@@ -0,0 +1,13 @@
+package com.render.example.render_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenderExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RenderExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/render/example/render_example/RenderExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.render.example.render_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenderExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
