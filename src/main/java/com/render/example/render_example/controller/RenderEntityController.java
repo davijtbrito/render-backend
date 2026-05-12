142017c2ba3f90fdc973119faad00eef9740360f
davijtbrito <47305119+davijtbrito@users.noreply.github.com>
2026-05-13 00:11:06 +0100

    adding record

--- src/main/java/com/render/example/render_example/controller/RenderEntityController.java
+++ src/main/java/com/render/example/render_example/controller/RenderEntityController.java
@@ -3,6 +3,8 @@ package com.render.example.render_example.controller;
 import com.render.example.render_example.model.RenderEntity;
 import com.render.example.render_example.repository.RenderEntityRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +23,10 @@ public class RenderEntityController {
     @GetMapping
     public List<RenderEntity> getAllRenderEntities() {
         return repository.findAll();
+    }
+
+    @PostMapping
+    public RenderEntity createRenderEntity(@RequestBody RenderEntity entity) {
+        return repository.save(entity);
     }
 }
