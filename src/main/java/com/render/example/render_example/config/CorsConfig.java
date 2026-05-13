99842d2eb288fccc00c71fe93d4d269c2246f0bf
davijtbrito <47305119+davijtbrito@users.noreply.github.com>
2026-05-13 14:20:00 +0100

    adding profile

---
+++ src/main/java/com/render/example/render_example/config/CorsConfig.java
@@ -0,0 +1,19 @@
+package com.render.example.render_example.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://render-frontend-v6af.onrender.com")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
